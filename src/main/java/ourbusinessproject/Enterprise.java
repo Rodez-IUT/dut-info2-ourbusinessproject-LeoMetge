eadb4df927867dd10e3119d8a10ce7fb8f37d69f
Léo Metge <metge.leo@gmail.com>
2020-02-03 14:14:12 +0100

    fix 1.2.5 classe Enterprise

---
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -0,0 +1,71 @@
+package ourbusinessproject;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+public class Enterprise {
+
+    /*
+     * Nom de l'entreprise
+     */
+    @NotEmpty
+    private String name;
+
+    /*
+     * description de l'entreprise
+     */
+    @Length(min = 10)
+    private String description;
+
+    /*
+     * Nom du contact
+     */
+    @NotEmpty
+    private String ContactName;
+
+    /*
+     * mail du contact
+     */
+    @NotEmpty
+    @Email
+    private String ContactEmail;
+
+
+    public Enterprise() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactName() {
+        return ContactName;
+    }
+
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return ContactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        ContactEmail = contactEmail;
+    }
+}
